77cc576bd42556a9cca2679af9bdedc867a47266
Muralidhar-3 <Muralidharmuthangi@gmail.com>
2024-05-17 19:35:16 +0530

    Counter Project Done

---
+++ counter in react/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    counter++;
+    setCounter(counter);
+    console.log(counter);
+  };
+
+  const decreaseValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+    console.log(counter);
+  };
+
+  return (
+    <>
+      <h1>Counter in React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Increase value</button>
+      <br />
+      <button onClick={decreaseValue}>Decrease value</button>
+    </>
+  );
+}
+
+export default App;
